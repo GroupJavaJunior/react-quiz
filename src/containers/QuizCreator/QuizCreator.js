91c3b70dd6e7a3f0cba1b28173b2641d333b0b16
m.shestakov <m.shestakov@itransition.com>
2020-07-04 01:39:37 +0300

    Add router

---
+++ src/containers/QuizCreator/QuizCreator.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './QuizCreator.module.css';
+
+export default class QuizCreator extends React.Component {
+  render() {
+    return (
+      <div className={classes.QuizCreator}>
+        <h1>Quiz Creator</h1>
+      </div>
+    )
+  }
+}
